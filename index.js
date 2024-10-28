4943fe60e67546955268df7525b17a4afa1d9d4f
matssaaren <167091453+matssaaren@users.noreply.github.com>
2024-10-28 23:08:13 +0200

    Displayed an article by slug

--- index.js
+++ index.js
@@ -43,6 +43,21 @@ app.get('/', (req, res) => {
     });
 });
 
+// Show article by this slug
+app.get('/article/:slug', (req, res) => {
+    let query = `SELECT * FROM article WHERE slug='${req.params.slug}'`;
+    let article;
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        article = result[0];
+        console.log(article);
+        res.render('article', {
+            article: article
+        });
+    });
+});
+
+
 app.listen(3003, () => {
     console.log('App is started at http://localhost:3003')
 })
