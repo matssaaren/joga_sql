df02f79cee91bbb26c28294b52ce0d866c04204e
matssaaren <167091453+matssaaren@users.noreply.github.com>
2024-10-28 23:04:26 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-const path = require('path')
 const hbs = require('express-handlebars');
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
@@ -29,6 +29,19 @@ con.connect((err) => {
     if (err) throw err;
     console.log('Connected to joga_mysql db')
 })
+
+// show all articles - index page
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = [];
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result;
+        res.render('index', {
+            articles: articles
+        });
+    });
+});
 
 app.listen(3003, () => {
     console.log('App is started at http://localhost:3003')
